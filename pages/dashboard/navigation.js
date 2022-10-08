7b4a3b3bd907691a556a753c2ee8e5bd1089bfa5
Ricardo Rivera <ricardorivera@Ricardos-MacBook-Pro.local>
2022-10-08 13:35:42 -0700

    setup prisma, on to post request handler

--- pages/dashboard/navigation.js
+++ pages/dashboard/navigation.js
@@ -1,7 +1,7 @@
 import DashboardLayout from './layout.js';
 import styles from '../../styles/Home.module.css';
 import dynamic from 'next/dynamic';
-
+import prisma from '../../utils/db.tsx'
 export default function Navigation() {
   const MapWithNoSSR = dynamic(() => import("../../components/Map.js"), {
     ssr: false,
@@ -13,11 +13,14 @@ export default function Navigation() {
   )
 }
 
-
 Navigation.getLayout = function getLayout(navigation) {
   return (
     <DashboardLayout>
       {navigation}
     </DashboardLayout>
   )
+}
+
+export async function getStaticProps(context) {
+
 }
