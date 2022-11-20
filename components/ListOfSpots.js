3cef1389085b52df73d15b77e30d7a86276da4f4
Ricardo Rivera <ricardorivera@Ricardos-MacBook-Pro.local>
2022-11-19 18:54:09 -0800

    fixed padding in list

--- components/ListOfSpots.js
+++ components/ListOfSpots.js
@@ -12,19 +12,21 @@ export default function ListOfSpots({ spots }) {
     <>
       {
         allSpots.map((spot) => (
-          <div key={spot.id} className={styles.lust}>
-            <div style={{ marginLeft: 16 }}>
-              <h1 className={styles.namen}>{spot.name}</h1>
-              <p className={styles.descy} >{spot.description}</p>
-              <a style={{
-                padding: '6px', backgroundColor: '#3264a8', borderRadius: '12px', borderWidth: '2px',
-                borderColor: 'buttonborder', borderStyle: 'outset', color: 'whitesmoke'
-              }}
-                href={`https://maps.google.com/?q=${spot.lat},${spot.lon}`}
-              >Directions</a>
-            </div>
-            <div>
-              <img src={spot.imagePath} className={styles.pikture} />
+          <div style={{ paddingBottom: 12 }}>
+            <div key={spot.id} className={styles.lust}>
+              <div style={{ marginLeft: 16 }}>
+                <h1 className={styles.namen}>{spot.name}</h1>
+                <p className={styles.descy} >{spot.description}</p>
+                <a style={{
+                  padding: '6px', backgroundColor: '#3264a8', borderRadius: '12px', borderWidth: '2px',
+                  borderColor: 'buttonborder', borderStyle: 'outset', color: 'whitesmoke'
+                }}
+                  href={`https://maps.google.com/?q=${spot.lat},${spot.lon}`}
+                >Directions</a>
+              </div>
+              <div>
+                <img src={spot.imagePath} className={styles.pikture} />
+              </div>
             </div>
           </div>
         ))
